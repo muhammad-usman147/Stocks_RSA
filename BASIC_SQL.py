97cba95f213e2ce0e04e483d8f4276aa1100d756
Muhammad Usman Shakeel <35249668+muhammad-usman147@users.noreply.github.com>
2020-11-22 02:07:14 -0800

    insert query added

--- BASIC_SQL.py
+++ BASIC_SQL.py
@@ -2,12 +2,15 @@ import pypyodbc
 print("success")
 
 conenction = pypyodbc.connect('Driver={SQL Server};Server=.;Database=MetalTiger')
+#insert query
 
 cursor = conenction.cursor()
-
+insert = "INSERT INTO ShareMaster(Code,Name,StockType) values('k','kk','kkk')"
+cursor.execute(insert)
+cursor.commit()
 cursor.execute("SELECT * FROM ShareMaster")
 s = ''
 for row in cursor:    
-    print(type(row))   
-    break
+    print(row)   
+    
 conenction.close()   
